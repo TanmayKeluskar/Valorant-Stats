8c874a3db37bea4e192365df407e9abafdd8431e
Tanmay Keluskar <keluskar.tanmay@gmail.com>
2023-01-20 12:19:25 +0530

    reworked the design with new updates

--- src/components/MainUi.jsx
+++ src/components/MainUi.jsx
@@ -27,18 +27,20 @@ export default function MainUi() {
         "ares": "55d8a0f4-4274-ca67-fe2c-06ab45efdf58",
         "vandal": "9c82e19d-4575-0200-1a81-3eacf00cf872",
         "bulldog": "ae3de142-4d85-2547-dd26-4e90bed35cf7",
-        "Cypher": "117ed9e3-49f3-6512-3ccf-0cada7e3823b",
-        "Sova": "ded3520f-4264-bfed-162d-b080e2abccf9",
-        "Killjoy": "1e58de9c-4950-5125-93e9-a0aee9f98746",
-        "Viper": "707eab51-4836-f488-046a-cda6bf494859",
-        "Phoenix": "eb93336a-449b-9c1b-0a54-a891f7921d69",
-        "Astra": "41fb69c1-4189-7b37-f117-bcaf1e96f1bf",
-        "Brimstone": "9f0d8ba9-4140-b941-57d3-a7ad57c6b417",
-        "Yoru": "7f94d92c-4234-0a36-9646-3a87eb8b5c89",
-        "Sage": "569fdd95-4d10-43ab-ca70-79becc718b46",
-        "Reyna": "a3bfb853-43b2-7238-a4f1-ad90e9e46bcc",
-        "Omen": "8e253930-4c05-31dd-1b6c-968525494517",
-        "Jett": "add6443a-41bd-e414-f6ad-e58d267f4e95"
+        "phantom": "ee8e8d15-496b-07ac-e5f6-8fae5d4c7b1a",
+        "judge": "ec845bf4-4f79-ddda-a3da-0db3774b2794",
+        "bucky": "910be174-449b-c412-ab22-d0873436b21b",
+        "frenzy": "44d4e95c-4157-0037-81b2-17841bf2e8e3",
+        "classic": "29a0cfab-485b-f5d5-779a-b59f85e204a8",
+        "ghost": "1baa85b4-4c70-1284-64bb-6481dfc3bb4e",
+        "sheriff": "e336c6b8-418d-9340-d77f-7a9e4cfe0702",
+        "shorty": "42da8ccc-40d5-affc-beec-15aa47b42eda",
+        "operator": "a03b24d3-4319-996d-0f8c-94bbfba1dfc7",
+        "guardian": "4ade7faa-4cf1-8376-95ef-39884480959b",
+        "marshal": "c4883e50-4494-202c-3ec3-6b8a9284f00b",
+        "spectre": "462080d1-4035-2937-7c09-27aa2a5c27a7",
+        "stinger": "f7e1b454-4ad4-1063-ec0a-159e56b58941",
+        "melee": "2f59173c-4bed-b6c3-2191-dea9b58be9c7"
     }
 
     // search type to be passed in the api url
@@ -103,8 +105,6 @@ export default function MainUi() {
                                 <select name="Search" id="search-drop" ref={getAttribute} onChange={(e) => { handleSearch(e) }}>
                                     <option value="agents" selected>Agent</option>
                                     <option value="weapons" >Weapon</option>
-                                    <option value="mercedes">Mercedes</option>
-                                    <option value="audi">Audi</option>
                                 </select>
                                 <div id="input-form">
                                     <input id="search-bar" type="text" placeholder="Name of the Agent" onChange={(e) => { setSearchValue(e.target.value); }} value={searchValue} />
--- src/components/RenderCard/RenderCard.jsx
+++ src/components/RenderCard/RenderCard.jsx
@@ -1,24 +1,54 @@
 import React from 'react'
+import './RenderCard.css'
 
 export default function RenderCard(props) {
+    const displayData = props.card.data
     return (
         <div className="search-result">
-            {props.card.data ?
+            {displayData ?
                 <div className="agent-info">
                     <div className="agent-looks">
                         <div id="display-icon">
-                            <img src={props.card.data.displayIcon != null ? props.card.data.displayIcon : ""} />
-                            <p>{props.card.data.isPlayableCharacter != null ? "Playable Character" : "Non-  Playable Character"}</p>
+                            <img src={displayData.displayIcon != null ? displayData.displayIcon : ""} />
+                            <p>{displayData.isPlayableCharacter != null ? "Playable Character" : "Non-  Playable Character"}</p>
                         </div>
-                        <div id="agent-name">{props.card.data.displayName != null ? props.card.data.displayName.toUpperCase() : ""}
+                        <div id="agent-name">{displayData.displayName != null ? displayData.displayName.toUpperCase() : ""}
                         </div>
                     </div>
+                    <div className="agent-description">
+                        {displayData.description != null ? <p> {displayData.description}</p> : <p>No Role</p>}
+                    </div>
                     <div className="info-items">
-                        {props.card.data.description != null ? <p> {props.card.data.description}</p> : ""}
                         <p class="sub-heads">ROLES</p>
-                        {props.card.data.role != null ? <img src={props.card.data.role.displayIcon} /> : ""}
-                        {props.card.data.role != null ? <p><u>{props.card.data.role.displayName}</u></p> : ""}
-                        {props.card.data.role != null ? <p>{props.card.data.role.description}</p> : <p>No Role</p>}
+                        <hr />
+                        {displayData.role != null ?
+                            <div class="display-item"><img src={displayData.role.displayIcon} /><span>{displayData.role.displayName.toUpperCase()}</span>
+                                <p>{displayData.role.description}</p>
+                            </div> : <p>No Role</p>
+                        }
+                        <p class="sub-heads">Abilities</p>
+                        <hr />
+                        {displayData.abilities != null ?
+                            displayData.abilities.map(ability => {
+                                return (
+                                    <>
+                                        <div class="display-item"><img src={ability.displayIcon} />
+                                            <span>{ability.displayName.toUpperCase()}</span>
+                                            <p>{ability.description}</p>
+                                            <p>- SLOT: {ability.slot}</p>
+                                        </div>
+                                    </>
+                                )
+                            }) : <p>No Abilities</p>
+                        }
+                        <div className="display-voice">
+                            <p class="sub-heads">Voice</p>
+                            {displayData.voiceLine.mediaList != null ?
+                                <div class="audioStripe">
+                                    <audio controls controlslist="nodownload noplaybackrate" src={displayData.voiceLine.mediaList[0].wave}>Play</audio>
+                                </div> : <p>No Role</p>
+                            }
+                        </div>
                     </div>
                 </div>
                 : ""}
